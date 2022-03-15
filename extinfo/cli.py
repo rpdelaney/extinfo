eba7b970f68cd578fe74f99654208e7225142bcc
Ryan Delaney <ryan.patrick.delaney+github@protonmail.com>
2022-03-15 15:48:51 -0400

    Split fileinfo_com into own extractor

--- extinfo/cli.py
+++ extinfo/cli.py
@@ -2,10 +2,13 @@ import sys
 
 import click
 import deal
+from requests.exceptions import HTTPError
 
-from .scraper import _fetch
+from extinfo.extractors import fileinfo_com
 
 deal.activate()
+
+__EXTRACTORS__ = [fileinfo_com]
 
 
 @click.command(
@@ -14,8 +17,14 @@ deal.activate()
 @click.version_option()
 @click.argument("extension", type=str)
 def cli(extension: str) -> int:
-    result = _fetch(extension)
-    print(result)
+    for extractor in __EXTRACTORS__:
+        try:
+            result = extractor.extract(extension)
+        except HTTPError as e:
+            print(str(e))
+        else:
+            print(f"From {extractor.SITE}\n")
+            print(result)
 
     return 0
 
--- extinfo/extractors/fileinfo_com.py
+++ extinfo/extractors/fileinfo_com.py
@@ -1,17 +1,30 @@
+import urllib.parse
+
 import requests
 from bs4 import BeautifulSoup as bs
-from requests.exceptions import HTTPError
+
+SITE = "fileinfo.com"
+PATH = "/extension/"
 
 
-def _fetch(extension: str) -> str:
-    url = f"https://fileinfo.com/extension/{extension}"
+def extract(extension: str) -> str:
+    soup = _fetch(extension)
+    return _parse(soup)
+
+
+def _fetch(extension: str) -> bs:
+    url = urllib.parse.urljoin(f"https://{SITE}", f"{PATH}/{extension}")
     r = requests.get(url)
-    try:
-        r.raise_for_status()
-    except HTTPError as e:
-        return str(e)
+    r.raise_for_status()
 
     soup = bs(r.text, "html.parser")
+
+    return soup
+
+
+def _parse(soup: bs) -> str:
+    # TODO: make this not horrific and awful
+    #
     headers = soup.find_all("h2")
     header0 = headers[0].text.strip()
     header1 = headers[1].text.strip()
@@ -23,7 +36,6 @@ def _fetch(extension: str) -> str:
     )
     result = "\n\n".join(
         [
-            f"From {url}",
             f"# {header0}",
             f"{info0}\n",
             f"# {header1}",
